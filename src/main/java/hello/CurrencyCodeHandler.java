3b4eb55c98e9bf789b5c0538713f349bd6e6aee3
i-savin <caba1983@gmail.com>
2015-09-27 00:42:53 +0300

    Initial commit

---
+++ src/main/java/hello/CurrencyCodeHandler.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Created by isavin on 26.09.15.
+ */
+public class CurrencyCodeHandler extends DefaultHandler {
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+
+    }
+
+}
